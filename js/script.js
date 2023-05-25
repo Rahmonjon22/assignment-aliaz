3a41431f4aea535fe5c6adf46072c4b9ed384db1
Rahmonjon22 <rahmonjon2@yahoo.com>
2023-05-25 06:46:45 +0200

    swipers

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ var swiper = new Swiper(".mySwiper", {
     grabCursor: true,
     loop: true,
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-});
+  });
+  
